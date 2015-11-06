6a328652f8bca1ca5d6c40ac174977f733eb5434
bpeters <brennen.peters@gmail.com>
2015-11-05 21:04:45 -0600

    Move get bunch to router

--- src/containers/bunch.js
+++ src/containers/bunch.js
@@ -1,8 +1,6 @@
 'use strict';
 
 var React = require('react-native');
-var Parse = require('parse/react-native');
-var ParseReact = require('parse-react/react-native');
 var _ = require('lodash');
 var Firebase = require('firebase');
 
@@ -28,34 +26,20 @@ var Styles = StyleSheet.create({
 });
 
 module.exports = React.createClass({
-  mixins: [ParseReact.Mixin],
   propTypes: {
     navigator: React.PropTypes.object,
     route: React.PropTypes.object,
     user: React.PropTypes.object,
     menuButton: React.PropTypes.object,
   },
-  observe: function() {
-    return {
-      bunches: (new Parse.Query('Bunch2User'))
-        .equalTo('user', this.props.user)
-        .equalTo('isMain', true)
-        .include("bunch"),
-    };
-  },
   getInitialState: function(){
     return {
       messages: []
     }
   },
-
-
- 
-
-
   componentWillReceiveProps:  function(nextProps){
     console.log('here')
-    var url = 'https://bunches.firebaseio.com/chat/' + nextProps.bunch.id.objectId;
+    var url = 'https://bunches.firebaseio.com/chat/' + nextProps.route.bunch.id.objectId;
     var messages = _.cloneDeep(this.state.messages);
     console.log(url)
     this.state.messenger = new Firebase(url);
@@ -78,10 +62,7 @@ module.exports = React.createClass({
 
   render: function() {
     
-    var bunch = _.chain(this.data.bunches)
-      .first()
-      .get('bunch')
-      .value();      
+      
 
      
 
--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var React = require('react-native');
+var Parse = require('parse/react-native');
+var ParseReact = require('parse-react/react-native');
 
 var routes = require('./routes');
 var SideMenu = require('./containers/sideMenu');
@@ -11,10 +13,24 @@ var {
 } = React;
 
 module.exports= React.createClass({
+  mixins: [ParseReact.Mixin],
   propTypes: {
     user: React.PropTypes.object,
   },
+  observe: function() {
+    return {
+      bunches: (new Parse.Query('Bunch2User'))
+        .equalTo('user', this.props.user)
+        .equalTo('isMain', true)
+        .include("bunch"),
+    };
+  },
   renderScene: function(route, navigator) {
+    route.bunch = _.chain(this.data.bunches)
+      .first()
+      .get('bunch')
+      .value();
+
     var Component = route.component;
 
     if (route.hasSideMenu) {
