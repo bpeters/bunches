b1885904c891c4bb50fba3c166b71e7437d12af9
bpeters <brennen.peters@gmail.com>
2015-12-16 00:10:03 -0600

    Add new hashtag components

---
+++ src/components/hashtagContainer.js
@@ -0,0 +1,120 @@
+'use strict';
+
+var React = require('react-native');
+var _ = require('lodash');
+
+var EnlargePhoto = require('../containers/enlargePhoto');
+var ChatCard = require('../elements/chatCard');
+
+var defaultStyles = require('../styles');
+
+var {
+  View,
+  TouchableOpacity,
+  Text,
+  ListView,
+  StyleSheet,
+  Image,
+} = React;
+
+var Styles = StyleSheet.create({
+  container: {
+    height: defaultStyles.bodyHeight,
+    paddingTop: defaultStyles.navBarHeight,
+  },
+  loadMore: {
+    width: defaultStyles.bodyWidth - 32,
+    marginTop: 32,
+    marginBottom: 32,
+    marginLeft: 16,
+    alignItems: 'center',
+  },
+  loadMoreText: {
+    fontFamily: 'Roboto-Regular', 
+    color: defaultStyles.medium,
+  },
+});
+
+module.exports = React.createClass({
+  propTypes: {
+    navigator: React.PropTypes.object,
+    chats: React.PropTypes.array,
+    getProfileChats: React.PropTypes.func,
+    squashMessages: React.PropTypes.func,
+    getHashtagChats: React.PropTypes.func,
+  },
+  getInitialState: function() {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (r1, r2) => r1 !== r2
+      }),
+    };
+  },
+  onPressRow: function (rowData) {
+    var Chat = require('../containers/chat');
+
+    this.props.navigator.push({
+      name: 'chat',
+      component: Chat,
+      hasSideMenu: true,
+      chatId: rowData.chat.id
+    });
+  },
+  onAvatarPress: function (rowData) {
+    var Profile = require('../containers/profile');
+
+    var user = rowData.chat.get('createdBy');
+
+    this.props.getProfileChats(user);
+
+    this.props.navigator.push({
+      name: 'profile',
+      component: Profile,
+      username: user.attributes.name,
+      handle: user.attributes.handle,
+    });
+  },
+  onPressImage: function (imageURL) {
+    this.props.navigator.push({
+      name: 'enlarge photo',
+      component: EnlargePhoto,
+      hasSideMenu: false,
+      photo: imageURL,
+    });
+  },
+  onHashtagPress: function (word) {
+    this.props.getHashtagChats(word);
+  },
+  renderChatRow: function(rowData) {
+    return (
+      <ChatCard
+        rowData={rowData}
+        onPressRow={this.onPressRow}
+        onAvatarPress={this.onAvatarPress}
+        onPressImage={this.onPressImage}
+        squashMessages={this.props.squashMessages}
+        onHashtagPress={this.onHashtagPress}
+      />
+    );
+  },
+  renderChatFooter: function () {
+    return (
+      <View style={Styles.loadMore}>
+
+      </View>
+    );
+  },
+  render: function() {
+    return (
+      <View style={Styles.container}>
+        <ListView
+          dataSource={this.state.dataSource.cloneWithRows(this.props.chats)}
+          renderRow={this.renderChatRow}
+          renderFooter={this.renderChatFooter}
+          automaticallyAdjustContentInsets={false}
+        />
+        {this.props.children}
+      </View>
+    );
+  }
+});
